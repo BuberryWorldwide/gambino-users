4950c94bb99a06c445a00ce13ffa4d60268917ef
BuberryWorldwide <officialbubies@gmail.com>
2025-08-25 19:43:38 +0000

    onboarding account creation done, next wallet gen

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,15 +1,24 @@
-// lib/api.js
+// lib/api.js (or wherever you create axios instance)
 import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.235:3001', // align with server
+  baseURL: process.env.NEXT_PUBLIC_API_BASE || 'http://192.168.1.235:3001',
   withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-  const t = getToken();
-  if (t) config.headers.Authorization = `Bearer ${t}`;
+  // Only add JWT token if there's NO Authorization header already set
+  // AND this isn't an onboarding request
+  const isOnboardingRequest = config.url?.includes('/onboarding/');
+  
+  if (!config.headers?.Authorization && !isOnboardingRequest) {
+    const t = getToken();
+    if (t) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${t}`;
+    }
+  }
   return config;
 });
 
